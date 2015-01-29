476414b4909eba47b2a14261bea4b2b631aa4107
jmallet <mallet@a11p19.fil.univ-lille1.fr>
2015-01-29 12:02:59 +0100

    ajout d'un commentaire bidon

--- src/main.java
+++ src/main.java
@@ -7,6 +7,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ok");
+		//un commentaire pour tester
+		System.out.println("coucou");
+		
 	}
 
 }
